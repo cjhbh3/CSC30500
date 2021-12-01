4e4425d27d8a77819e86606e5e9b9cf55365ea6c
CJ Hess <54945896+cjhbh3@users.noreply.github.com>
2021-12-01 10:25:02 -0600

    Update listIngredients.php

--- Project 3/listIngredients.php
+++ Project 3/listIngredients.php
@@ -1,9 +1,11 @@
 <?php
 
-$host = $_POST["host"];
-$user = $_POST["user"];
-$pass = $_POST["password"];
-$port = $_POST["port"];
+session_start();
+
+$host = $_SESSION["host"];
+$user = $_SESSION["user"];
+$pass = $_SESSION["passw"];
+$port = $_SESSION["port"];
 
 $recipeName = $_POST["recipeName"];
 
@@ -17,5 +19,32 @@ if ($conn->connect_error)
 else
         echo " connected!<br>";
 
+if (!$stmt = $conn->prepare("select * from Recipe where RecipeName = ?;")){
+        echo "Issue here" . htmlspecialchars($conn->error);
+}
 
+
+$stmt->bind_param("s", $recipeName);
+
+if (!$stmt->execute())
+        echo "Fail<br>";
+
+$stmt->bind_result($rName, $iName, $iQuantity);
+
+echo "<table border=1>";
+echo "<tr> <th>Recipe Name</th> <th>Ingredientt Name</th> <th>Quantity</th> </tr>";
+
+while($stmt->fetch() ) 
+    {
+        echo "<tr> <td>".$rName."</td>". 
+                  "<td>".$iName."</td>". 
+                  "<td>".$iQuantity."</td> </tr>";
+    }
+
+echo "</table>";
+
+echo ("All done!<br>");
+
+echo "<a href=/mainMenu.html>Main Menu</a>";
+$conn->close();
 ?>
